6b9f8f6b5eb0640a379d7dba814153acb4494b6b
muniira-03 <munirra.alsubaie@gmail.com>
2025-06-22 10:44:44 +0300

    Delete part2/hbnb directory

--- part2/hbnb/app.py
+++
@@ -1,9 +0,0 @@
-# app.py
-from flask import Flask
-from presentation.api import api_bp
-
-app = Flask(__name__)
-app.register_blueprint(api_bp, url_prefix="/api")
-
-if __name__ == "__main__":
-    app.run(debug=True)
