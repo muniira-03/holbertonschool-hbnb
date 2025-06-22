bac4ab66420216ec0d1a071fa1ae24af9f67b513
Muniira-03 <muniira.alsubaie@gmail.com>
2025-06-22 07:43:40 +0000

    upload files

---
+++ part2/run.py
@@ -0,0 +1,9 @@
+# part2/run.py
+from flask import Flask
+from hbnb.app.api.v1 import api_v1
+
+app = Flask(__name__)
+app.register_blueprint(api_v1)
+
+if __name__ == "__main__":
+    app.run(debug=True)
