12406d45701773b166f8db434b587294a3645db7
muniira-03 <munirra.alsubaie@gmail.com>
2025-06-30 11:17:46 +0300

    Update __init__.py

---
+++ part3/hbnb/__init__.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app(config_class):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+
+    return app
