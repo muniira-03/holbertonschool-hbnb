b3c7b0046d988d8674cfd9d25352dd66da63aeba
muniira-03 <munirra.alsubaie@gmail.com>
2025-07-01 10:38:26 +0300

    Update auth.py

--- part3/hbnb/app/api/v1/auth.py
+++ part3/hbnb/app/api/v1/auth.py
@@ -1,24 +1,21 @@
 from flask_restx import Namespace, Resource, fields
 from flask import request
 from flask_jwt_extended import create_access_token
-from hbnb.app.models.user import User
+from hbnb.models.user import User
+from hbnb import db
 
-ns = Namespace('auth', description='Authentication operations')
+ns = Namespace("auth", description="Auth operations")
 
-login_model = ns.model('Login', {
-    'email': fields.String(required=True),
-    'password': fields.String(required=True)
-})
+login_model = ns.model("Login", {"email": fields.String(required=True), "password": fields.String(required=True)})
 
-@ns.route('/login')
+@ns.route("/login")
 class Login(Resource):
     @ns.expect(login_model)
     def post(self):
-        data = request.get_json() 
-        user = User.query.filter_by(email=data['email']).first()
-        if user and user.check_password(data['password']):
-            claims = {"is_admin": user.is_admin}
-            access_token = create_access_token(identity=user.id, additional_claims=claims)
-            return {'access_token': access_token}, 200
-        return {'message': 'Invalid credentials'}, 401
+        data = request.get_json()
+        user = User.query.filter_by(email=data["email"]).first()
+        if not user or not user.check_password(data["password"]):
+            return {"message": "Invalid credentials"}, 401
 
+        access_token = create_access_token(identity=user.id, additional_claims={"is_admin": user.is_admin})
+        return {"access_token": access_token}, 200
