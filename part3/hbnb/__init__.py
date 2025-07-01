bac3d5fc6c6ebeb76f14356659f2d742edbe04de
muniira-03 <munirra.alsubaie@gmail.com>
2025-07-01 10:36:13 +0300

    Update __init__.py

--- part3/hbnb/__init__.py
+++ part3/hbnb/__init__.py
@@ -1,11 +1,26 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_restx import Api
+from .config import Config
 
 db = SQLAlchemy()
+jwt = JWTManager()
 
-def create_app(config_class):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    app.config.from_object(Config)
 
+    
     db.init_app(app)
+    jwt.init_app(app)
+
+    
+    from .api.v1 import api_v1
+    api = Api(app, version="1.0", title="HBnB API")
+    api.add_namespace(api_v1)
+
+    with app.app_context():
+        db.create_all()  
 
     return app
