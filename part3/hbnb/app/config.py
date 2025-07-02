cd5e93f5a83c8fef2aae5be762ab5582bc51eb5b
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-02 12:05:24 +0300

    Update config.py

--- part3/hbnb/app/config.py
+++ part3/hbnb/app/config.py
@@ -1,7 +1,7 @@
-Import os 
+import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///mydatabase.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key_here')
