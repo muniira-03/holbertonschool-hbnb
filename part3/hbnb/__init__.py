e3fda9c405f40a4d532bcd56a8d51400330685a2
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-02 12:01:32 +0300

    Update __init__.py

--- part3/hbnb/__init__.py
+++ part3/hbnb/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from .config import Config
+from hbnb.app.config import Config
 from hbnb.app.api.v1 import api_v1
 from hbnb.app.models.db import db  
 
