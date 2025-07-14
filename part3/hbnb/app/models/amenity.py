78b8ee0d5eaa1808f75483a689d6fec4fa29f0d2
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-14 12:15:22 +0300

    Update amenity.py

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -1,13 +1,20 @@
 from hbnb.app.models.db import db
-from hbnb.app.models.associations import place_amenities  
+from hbnb.app.models.associations import place_amenities
 
 class Amenity(db.Model):
+    __tablename__ = 'amenity'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
-    
 
     places = db.relationship(
         'Place',
         secondary=place_amenities,
         back_populates='amenities'
     )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
