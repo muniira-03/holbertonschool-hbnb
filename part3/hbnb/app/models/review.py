2f1ef0681741aaf908bd8288ae434c5e343cbb99
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-14 12:15:46 +0300

    Update review.py

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -1,8 +1,20 @@
 from hbnb.app.models.db import db
 
 class Review(db.Model):
+    __tablename__ = 'review'
+
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     place_id = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=False)
 
+    user = db.relationship('User', backref='reviews')
     place = db.relationship('Place', back_populates='reviews')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "user_id": self.user_id,
+            "place_id": self.place_id
+        }
