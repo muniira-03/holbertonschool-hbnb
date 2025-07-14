ac95cda629acb79b6b3c24ce2b134d5850a2968a
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-14 11:50:39 +0300

    Update __init__.py

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from flask_restx import Api
 from hbnb.app.config import Config
-from hbnb.app.models.db import db
-from hbnb.app.api.v1.users import ns as users_namespace
+from hbnb.api.v1 import api_v1_bp   
+from hbnb.app.models.db import db  
 
 jwt = JWTManager()
 
@@ -14,7 +13,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     jwt.init_app(app)
 
-    api = Api(app, version='1.0', title='HBnB API')
-    api.add_namespace(users_namespace, path='/api/v1/users')
+    
+    app.register_blueprint(api_v1_bp)
 
     return app
