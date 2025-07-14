7acb1adfb9d74e4782cea922eaea2288c8f473ca
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-14 12:16:12 +0300

    Update user.py

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -1,21 +1,14 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 from hbnb.app.models.db import db
 
 class User(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    
 
     def to_dict(self):
         return {
             "id": self.id,
-            "email": self.email,
-            "is_admin": self.is_admin
+            "username": self.username
         }
