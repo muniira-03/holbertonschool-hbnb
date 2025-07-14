c294b7d4dcf13d5fc78076eb3ce1cb7efde17a48
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-14 12:14:25 +0300

    Update associations.py

--- part3/hbnb/app/models/associations.py
+++ part3/hbnb/app/models/associations.py
@@ -3,5 +3,6 @@ from hbnb.app.models.db import db
 place_amenities = db.Table(
     'place_amenities',
     db.Column('place_id', db.Integer, db.ForeignKey('place.id'), primary_key=True),
-    db.Column('amenity_id', db.Integer, db.ForeignKey('amenity.id'), primary_key=True)
+    db.Column('amenity_id', db.Integer, db.ForeignKey('amenity.id'), primary_key=True),
+    extend_existing=True  
 )
