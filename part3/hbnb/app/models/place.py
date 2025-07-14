c23e4a207c33767a05ebe11ce86b6ed95e55628e
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-14 11:33:02 +0300

    Update place.py

--- part3/hbnb/app/models/place.py
+++ part3/hbnb/app/models/place.py
@@ -1,5 +1,10 @@
 from hbnb.app.models.db import db
 
+place_amenities = db.Table('place_amenities',
+    db.Column('place_id', db.Integer, db.ForeignKey('place.id'), primary_key=True),
+    db.Column('amenity_id', db.Integer, db.ForeignKey('amenity.id'), primary_key=True)
+)
+
 class Place(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
@@ -8,3 +13,14 @@ class Place(db.Model):
 
     user = db.relationship('User', backref='places')
     reviews = db.relationship('Review', backref='place', cascade='all, delete-orphan')
+    amenities = db.relationship('Amenity', secondary=place_amenities, backref='places')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "user_id": self.user_id,
+            "reviews": [review.to_dict() for review in self.reviews],
+            "amenities": [amenity.to_dict() for amenity in self.amenities],
+        }
