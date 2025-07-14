60fa3d89bf67493159c32a119edb76a15fe670cd
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-14 12:04:11 +0300

    Update amenity.py

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -2,11 +2,10 @@ from hbnb.app.models.db import db
 
 place_amenities = db.Table('place_amenities',
     db.Column('place_id', db.Integer, db.ForeignKey('place.id'), primary_key=True),
-    db.Column('amenity_id', db.Integer, db.ForeignKey('amenity.id'), primary_key=True),
-    extend_existing=True 
+    db.Column('amenity_id', db.Integer, db.ForeignKey('amenity.id'), primary_key=True)
 )
 
 class Amenity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
-    places = db.relationship('Place', secondary=place_amenities, backref='amenities')
+    places = db.relationship('Place', secondary=place_amenities, back_populates='amenities')
